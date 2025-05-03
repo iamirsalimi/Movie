b1da693da5b1b176c59a7df4c7904cdb424a3edb
iamirsalimi <wonderguy2003@gmail.com>
2025-05-03 21:15:57 +0330

    Watch List Page got responsive

--- src/Pages/Watchlist/WatchList.jsx
+++ src/Pages/Watchlist/WatchList.jsx
@@ -6,10 +6,10 @@ import WatchListMovieCard from '../../Components/WatchListMovieCard/WatchListMov
 
 export default function WatchList() {
   return (
-    <div className="py-4 pb-12 px-5 panel-box flex flex-col gap-7">
+    <div className="py-4 pb-12 px-5 panel-box flex flex-col gap-7 mb-16">
       <h2 className="text-gray-700 dark:text-white font-vazir text-xl">لیست تماشا</h2>
 
-      <div className="grid grid-cols-5 gap-x-4 gap-y-16">
+      <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-16">
         {movies.map(movie => (
           <WatchListMovieCard {...movie} />
         ))}
