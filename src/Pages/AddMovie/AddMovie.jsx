90a231bb2cd5387792c4c9e76b9f403a49076dd1
iamirsalimi <wonderguy2003@gmail.com>
2025-05-06 23:54:02 +0330

    tags added to Movie Page

--- src/Pages/AddMovie/AddMovie.jsx
+++ src/Pages/AddMovie/AddMovie.jsx
@@ -18,10 +18,13 @@ export default function AddMovie() {
     const [suggestedCheckbox, setSuggestedCheckbox] = useState(false)
 
     const [movieType, setMovieType] = useState('movie') // movie or series
+
     const [linkTitle, setLinkTitle] = useState()
     const [linkType, setLinkType] = useState('dubbed')
     const [links, setLinks] = useState([])
 
+    const [tagTitle, setTagTitle] = useState('')
+    const [tags, setTags] = useState([])
 
     const [genre, setGenre] = useState('')
     const [showGenres, setShowGenres] = useState(false)
@@ -50,6 +53,22 @@ export default function AddMovie() {
     const deleteLink = id => {
         let newLinks = links.filter(link => link.id !== id)
         setLinks(newLinks)
+    }
+
+    // add Tag
+    const addTag = e => {
+        if (tagTitle) {
+            let newTags = new Set(tags)
+            newTags.add(tagTitle)
+            setTags([...newTags])
+            setTagTitle('')
+        }
+    }
+
+    const deleteTag = tagTitle => {
+        let newTags = new Set(tags)
+        newTags.delete(tagTitle)
+        setTags([...newTags])
     }
 
     // add similar Movies
@@ -253,14 +272,6 @@ export default function AddMovie() {
                     <span className="absolute peer-focus:text-sky-500 transition-all -top-3 right-2 font-vazir px-2 text-light-gray dark:text-gray-600 bg-white dark:bg-secondary">محصول</span>
                 </div>
 
-                <div className="col-start-1 col-end-3 w-full relative select-none">
-                    <input
-                        type="text"
-                        className="w-full rounded-md p-3 border border-light-gray dark:border-gray-600 dark:bg-secondary bg-white text-light-gray dark:text-white outline-none peer focus:border-sky-500 focus:text-sky-500 transition-colors"
-                    />
-                    <span className="absolute peer-focus:text-sky-500 transition-all -top-3 right-2 font-vazir px-2 text-light-gray dark:text-gray-600 bg-white dark:bg-secondary">تگ ها</span>
-                </div>
-
                 <div className="w-full relative select-none">
                     <input
                         type="text"
@@ -381,6 +392,48 @@ export default function AddMovie() {
                                             <button
                                                 className="p-1 bg-red-500 hover:bg-blackred-600 transition-colors rounded-sm cursor-pointer"
                                                 onClick={e => deleteLink(link.id)}
+                                            >
+                                                <RxCross2 className="text-white dark:text-primary" />
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                {/* Tags */}
+                <div className="col-start-1 col-end-3 flex flex-col gap-5 bg-gray-100 dark:bg-primary rounded-lg py-2 px-3">
+                    <h3 className="w-full font-vazir text-gray-800 dark:text-white text-lg">ساخت تگ جدید</h3>
+                    <div className="w-full flex flex-col items-center gap-4">
+                        <div className="w-full py-5 px-4 border border-gray-200 dark:border-secondary rounded-md grid grid-cols-3 gap-5">
+                            <div className="col-start-1 col-end-3 w-full relative select-none">
+                                <input
+                                    type="text"
+                                    className="w-full rounded-md p-3 border border-light-gray dark:border-gray-600 dark:bg-primary bg-gray-100 text-light-gray dark:text-white outline-none peer focus:border-sky-500 focus:text-sky-500 transition-colors"
+                                    value={tagTitle}
+                                    onChange={e => setTagTitle(e.target.value)}
+                                />
+                                <span className="absolute peer-focus:text-sky-500 transition-all -top-3 right-2 font-vazir px-2 text-light-gray dark:text-gray-600 bg-gray-100 dark:bg-primary">عنوان تگ</span>
+                            </div>
+
+                            <button
+                                className="py-2 rounded-md bg-sky-500 hover:bg-sky-600 transition-colors text-white font-vazir cursor-pointer"
+                                onClick={addTag}
+                            >افزودن</button>
+                        </div>
+
+                        {tags.length !== 0 && (
+                            <div className="w-full flex flex-col items-center gap-2">
+                                <h3 className="w-full text-center font-vazir text-gray-800 dark:text-white text-lg">تگ های فیلم</h3>
+                                <div className="w-full flex flex-col items-center gap-2">
+                                    {tags.map(tag => (
+                                        <div className="w-full bg-gray-200 dark:bg-secondary flex items-center justify-between px-2 py-1 rounded-lg">
+                                                <h3 className="text-light-gray dark:text-white font-shabnam">{tag}</h3>
+                                            <button
+                                                className="p-1 bg-red-500 hover:bg-blackred-600 transition-colors rounded-sm cursor-pointer"
+                                                onClick={e => deleteTag(tag)}
                                             >
                                                 <RxCross2 className="text-white dark:text-primary" />
                                             </button>
