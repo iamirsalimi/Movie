238aad94e8708052b0e01d51a4b04593b59fdd9e
iamirsalimi <wonderguy2003@gmail.com>
2025-06-02 00:40:23 +0330

    now adminDashboard page in admin panel got fully dynamic

--- src/Pages/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/AdminDashboard/AdminDashboard.jsx
@@ -6,7 +6,6 @@ import dayjs from 'dayjs';
 import jalali from 'jalaliday';
 
 import UserActivityInfo from './../../Components/UserActivityInfo/UserActivityInfo'
-import DashboardTable from './../../Components/DashboardTable/DashboardTable'
 import AnnouncementElem from '../../Components/AnnouncementElem/AnnouncementElem';
 import AnnounceMentModal from '../../Components/AnnounceMentModal/AnnounceMentModal'
 import DeleteModal from '../../Components/DeleteModal/DeleteModal';
@@ -27,13 +26,15 @@ let apiData = {
     updateAnnouncementApi: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/Announcements?id=eq.',
     postAnnouncement: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/Announcements',
     getAnnouncementsApi: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/Announcements?select=*',
-    getApi: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/users?userToken=eq.',
+    getRequestsApi: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/requests?select=*',
+    getTicketsApi: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/tickets?select=*',
+    getUsersApi: 'https://xdxhstimvbljrhovbvhy.supabase.co/rest/v1/users?select=*',
     apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkeGhzdGltdmJsanJob3Zidmh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MDY4NTAsImV4cCI6MjA2MjQ4Mjg1MH0.-EttZTOqXo_1_nRUDFbRGvpPvXy4ONB8KZGP87QOpQ8',
     authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkeGhzdGltdmJsanJob3Zidmh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MDY4NTAsImV4cCI6MjA2MjQ4Mjg1MH0.-EttZTOqXo_1_nRUDFbRGvpPvXy4ONB8KZGP87QOpQ8'
 }
 
 export default function AdminDashboard() {
-    const [announcements, setAnnouncements] = useState([])
+    const [announcements, setAnnouncements] = useState(null)
     const [announcementsIsPending, setAnnouncementsIsPending] = useState(false)
     const [announcementError, setAnnouncementError] = useState(false)
     const [showAnnouncementModal, setShowAnnouncementModal] = useState(false)
@@ -42,6 +43,13 @@ export default function AdminDashboard() {
     const [announcementObj, setAnnouncementObj] = useState(null)
     const [isAdding, setIsAdding] = useState(false)
     const [showDeleteModal, setShowDeleteModal] = useState(false)
+    const [tickets, setTickets] = useState(null)
+    const [ticketsIsPending, setTicketsIsPending] = useState(false)
+    const [ticketsError, setTicketsError] = useState(false)
+    const [requests, setRequests] = useState(null)
+    const [requestsIsPending, setRequestsIsPending] = useState(false)
+    const [requestsError, setRequestsError] = useState(false)
+    const [users, setUsers] = useState(null)
 
     const [ipObj, setIpObj] = useState(null)
 
@@ -116,16 +124,6 @@ export default function AdminDashboard() {
             })
     }
 
-    useEffect(() => {
-        fetch('https://ipapi.co/json/')
-            .then(res => res.json())
-            .then(data => {
-                // console.log(data)
-                setIpObj(data)
-            })
-            .catch(err => console.error(err));
-    }, [])
-
     // getting all announcements
     useEffect(() => {
         const getAnnouncements = async () => {
@@ -141,7 +139,7 @@ export default function AdminDashboard() {
 
                 if (data.length > 0) {
                     setAnnouncements(data)
-                    setAnnouncementsIsPending(false)
+                    setAnnouncementsIsPending(null)
                 }
 
                 setAnnouncementError(false)
@@ -158,6 +156,120 @@ export default function AdminDashboard() {
         }
     }, [getAnnouncements, userObj])
 
+    // getting all tickets
+    useEffect(() => {
+        const getTickets = async () => {
+            try {
+                const res = await fetch(apiData.getTicketsApi, {
+                    headers: {
+                        'apikey': apiData.apikey,
+                        'Authorization': apiData.authorization
+                    }
+                })
+
+                const data = await res.json()
+
+                if (data.length > 0) {
+                    setTickets(data)
+                    setTicketsIsPending(null)
+                }
+
+                setTicketsError(false)
+            } catch (err) {
+                console.log('fetch error')
+                setTicketsError(err)
+                setTicketsIsPending(false)
+                setTickets(null)
+            }
+        }
+
+        // when the announcementsIsPending is null it means announcements has fetched 
+        if (announcementsIsPending == null) {
+            setTicketsIsPending(true)
+            getTickets()
+        }
+    }, [announcementsIsPending])
+
+    // getting all requests
+    useEffect(() => {
+        const getRequests = async () => {
+            try {
+                const res = await fetch(apiData.getRequestsApi, {
+                    headers: {
+                        'apikey': apiData.apikey,
+                        'Authorization': apiData.authorization
+                    }
+                })
+
+                const data = await res.json()
+
+                if (data.length > 0) {
+                    setRequests(data.sort((a, b) => {
+                        let aDate = new Date(a.updated_at).getTime()
+                        let bDate = new Date(b.updated_at).getTime()
+                        return bDate - aDate
+                    }).slice(0, 5))
+
+                    setRequestsIsPending(null)
+                }
+
+                setRequestsError(false)
+            } catch (err) {
+                console.log('fetch error')
+                setRequestsError(err)
+                console.log(err)
+                setRequestsIsPending(false)
+                setRequests(null)
+            }
+        }
+
+        // when the ticketsIsPending is null it means tickets has fetched 
+        if (ticketsIsPending == null) {
+            setRequestsIsPending(true)
+            getRequests()
+        }
+    }, [ticketsIsPending])
+
+    // getting all Users
+    useEffect(() => {
+        const getAllUsers = async () => {
+            try {
+                const res = await fetch(apiData.getUsersApi, {
+                    headers: {
+                        'apikey': apiData.apikey,
+                        'Authorization': apiData.authorization
+                    }
+                })
+
+                const data = await res.json()
+
+                if (data.length > 0) {
+                    setUsers(data)
+                }
+            } catch (err) {
+                console.log('fetch error', err)
+            }
+        }
+
+        // when the requestsIsPending is null it means requests has fetched 
+        if (requestsIsPending == null) {
+            getAllUsers()
+        }
+    }, [requestsIsPending])
+
+    useEffect(() => {
+        if(requestsIsPending == null){
+            fetch('https://ipapi.co/json/')
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data)
+                    setIpObj(data)
+                })
+                .catch(err => console.error(err));
+
+        }
+    }, [requestsIsPending])
+
     const getDate = date => {
         let registerDate = new Date(date)
         let persianDate = dayjs(registerDate).calendar('jalali').locale('fa').format('YYYY/MM/DD')
@@ -168,11 +280,11 @@ export default function AdminDashboard() {
     return (
         <>
             <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                <UserActivityInfo color='from-orange-400 to-orange-600' title="درخواست ها" value={20}>
+                <UserActivityInfo color='from-orange-400 to-orange-600' title="تیکت ها" value={tickets?.length || 0}>
                     <TbTicket className="text-white text-xl xl:text-2xl" />
                 </UserActivityInfo>
 
-                <UserActivityInfo color='from-purple-600 to-purple-800' title="مطالب درخواستی" value={23}>
+                <UserActivityInfo color='from-purple-600 to-purple-800' title="مطالب درخواستی" value={requests?.length || 0}>
                     <IoIosAddCircleOutline className="text-white text-xl xl:text-2xl" />
                 </UserActivityInfo>
 
@@ -180,7 +292,7 @@ export default function AdminDashboard() {
                     <PiUserFocusFill className="text-white text-xl xl:text-2xl" />
                 </UserActivityInfo>
 
-                <UserActivityInfo color='from-green-400 to-green-600' title="كاربر ها" value={25}>
+                <UserActivityInfo color='from-green-400 to-green-600' title="كاربر ها" value={users?.length || 0}>
                     <FaUsers className="text-white text-xl xl:text-2xl" />
                 </UserActivityInfo>
             </div>
@@ -199,7 +311,7 @@ export default function AdminDashboard() {
                 <div className="w-full flex flex-col items-center gap-2">
                     {!announcementsIsPending && (
                         <>
-                            {announcements.length > 0 ? announcements.map(announcement => (
+                            {announcements?.length > 0 ? announcements?.map(announcement => (
                                 <AnnouncementElem key={announcement.id} {...announcement} getDate={getDate} announcementObj={announcement} setShowAnnouncementModal={setShowAnnouncementModal} setUpdateFlag={setUpdateFlag} setAnnouncementObj={setAnnouncementObj} setShowDeleteModal={setShowDeleteModal} adminFlag />
                             )) : (
                                 // it means we just wanna show base announcement elem which only says there is no announcements 
@@ -230,7 +342,40 @@ export default function AdminDashboard() {
                             <h2 className="font-vazir text-gray-800 dark:text-white text-lg">تیکت ها</h2>
                         </div>
 
-                        <DashboardTable tableHeaders={['#', 'عنوان تیکت', 'دپارتمان', 'بروزرسانی']} />
+                        <div className="w-full py-3 px-2 rounded-lg border border-gray-200 dark:border-white/5 overflow-x-scroll lg:overflow-x-hidden">
+                            <table className="w-full">
+                                <thead className="min-w-full">
+                                    <tr className="py-1 px-2 border-b border-gray-200 dark:border-white/5" >
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">#</th>
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">عنوان تیکت</th>
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">دپارتمان</th>
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">بروزرسانی</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="min-w-full">
+                                    {!ticketsIsPending && tickets?.map(ticket => (
+                                        <tr className="py-1 px-2 odd:bg-gray-100 dark:odd:bg-primary text-center" >
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{ticket.id}</td>
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{ticket.userName}</td>
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{ticket.subject}</td>
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{ticket.status == 'pending' ? 'در حال بررسی ' : ticket.status == 'answered' ? 'جواب داده شده' : 'بسته شده'}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+                            {ticketsIsPending == null && tickets?.length == 0 && (
+                                <h2 className="text-center text-red-500 font-vazir text-sm mt-4">تا كنون تیکتی توسط کاربران ثبت نشده</h2>
+                            )}
+
+                            {ticketsIsPending && (
+                                <h2 className="text-center text-red-500 font-vazir text-sm mt-4">در حال دریافت تیکت ها ... </h2>
+                            )}
+
+                            {ticketsError && (
+                                <h2 className="text-center text-red-500 font-vazir text-sm mt-4">در دریافت اطلاعات از سرور مشکل بر خوردیم لطفا صفحه را رفرش کنید</h2>
+                            )}
+                        </div>
                     </div>
 
                     <div className="py-3 px-4 flex flex-col gap-4 panel-box">
@@ -242,11 +387,42 @@ export default function AdminDashboard() {
                             <h2 className="font-vazir text-gray-800 dark:text-white text-lg">درخواست ها</h2>
                         </div>
 
-                        <DashboardTable tableHeaders={['#', 'عنوان', 'تاریخ ثبت', 'وضعیت']} />
+                        <div className="w-full py-3 px-2 rounded-lg border border-gray-200 dark:border-white/5 overflow-x-scroll lg:overflow-x-hidden">
+                            <table className="w-full">
+                                <thead className="min-w-full">
+                                    <tr className="py-1 px-2 border-b border-gray-200 dark:border-white/5" >
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">#</th>
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">عنوان</th>
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">تاریخ ثبت</th>
+                                        <th className="py-1 pb-3 px-2 text-light-gray dark:text-gray-400">وضعیت</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="min-w-full">
+                                    {!requestsIsPending && requests?.map(request => (
+                                        <tr className="py-1 px-2 odd:bg-gray-100 dark:odd:bg-primary text-center" >
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{request.id}</td>
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{request.title}</td>
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{getDate(request.created_at)}</td>
+                                            <td className="py-1 pb-3 px-2 font-vazir text-sm text-light-gray dark:text-gray-400">{request.status == 'pending' ? 'در حال بررسی ' : request.status == 'approved' ? 'قبول شده' : 'رد شده'}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+                            {requestsIsPending == null && requests?.length == 0 && (
+                                <h2 className="text-center text-red-500 font-vazir text-sm mt-4">تا كنون درخواستي توسط کاربران ثبت نشده</h2>
+                            )}
+
+                            {requestsIsPending && (
+                                <h2 className="text-center text-red-500 font-vazir text-sm mt-4">در حال دریافت تیکت ها ... </h2>
+                            )}
+
+                            {requestsError && (
+                                <h2 className="text-center text-red-500 font-vazir text-sm mt-4">در دریافت اطلاعات از سرور مشکل بر خوردیم لطفا صفحه را رفرش کنید</h2>
+                            )}
+                        </div>
                     </div>
-
                 </div>
-
                 <div className="w-full lg:w-1/4 flex flex-col gap-5">
 
                     <div className="p-4 flex flex-col gap-4 panel-box">
